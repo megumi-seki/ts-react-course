49b68c75ca549d7bdb1240bcc81724d3da023d02
megumi-seki <seki.megumi.v0@tufs.ac.jp>
2025-06-15 22:46:45 +0900

    learn react useReducer + ts

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,17 +1,57 @@
-import type { ReactNode } from "react"
+import { type ChangeEvent, type ReactNode, useReducer } from "react"
 
-type CounterProps = {
-    setCount: React.Dispatch<React.SetStateAction<number>>,
-    children: ReactNode,
+const initState = { count: 0, text: "" }
+
+const enum REDUCER_ACTION_TYPE {
+    INCREMENT,
+    DECREMENT,
+    NEW_INPUT,
 }
 
-const Counter = ({ setCount, children }: CounterProps) => {
+type ReducerAction = {
+    type: REDUCER_ACTION_TYPE,
+    payload?: string,
+}
+
+const reducer = (state: typeof initState, action: ReducerAction): typeof initState => {
+    switch(action.type) {
+        case REDUCER_ACTION_TYPE.INCREMENT:
+            return { ...state, count: state.count + 1}
+        case REDUCER_ACTION_TYPE.DECREMENT:
+            return { ...state, count: state.count - 1}
+        case REDUCER_ACTION_TYPE.NEW_INPUT:
+            return { ...state, text: action.payload ?? ""}
+        default: 
+            throw new Error()
+    }
+}
+
+type ChildrenType = {
+    children: (num: number) => ReactNode
+}
+
+const Counter = ({ children }: ChildrenType) => {
+    const [state, dispatch] = useReducer(reducer, initState)
+
+    const increment = () => dispatch({ type: REDUCER_ACTION_TYPE.INCREMENT })
+    const decrement = () => dispatch({ type: REDUCER_ACTION_TYPE.DECREMENT })
+    const handleTextInput = (e: ChangeEvent<HTMLInputElement>) => {
+        dispatch(
+            { type: REDUCER_ACTION_TYPE.NEW_INPUT,
+                payload: e.target.value
+             })
+    }
 
     return (
         <>
-            <h1>{children}</h1>
-            <button onClick={() => setCount(c => c + 1)}>+</button>
-            <button onClick={() => setCount(c => c - 1)}>-</button>
+            <h1>{children(state.count)}</h1>
+            <div>
+                <button onClick={increment}>+</button>
+                <button onClick={decrement}>-</button>
+            </div>
+            <label htmlFor="MyInput">MyInput</label>
+            <input type="text" onChange={handleTextInput} id="MyInput"/>
+            <h2>{state.text}</h2>
         </>
     )
 }
