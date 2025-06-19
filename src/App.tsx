9af7027ad605c4189cce620f4e091831af62262b
megumi-seki <seki.megumi.v0@tufs.ac.jp>
2025-06-19 16:10:31 +0900

    react + ts project 1st part

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
-import Counter from "./components/Counter";
-import { CounterProvider } from "./context/CounterContext";
-import { initState } from "./context/CounterContext";
 
 function App() {
 
   return(
     <>
-      <CounterProvider count={initState.count} text={initState.text}>
-        <Counter>{(num: number) => <>Current Count: {num}</>}</Counter>
-      </CounterProvider>
+
     </>
   )
 }
